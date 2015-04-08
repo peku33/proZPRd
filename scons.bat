a530eeefeeefab7b08e7d213cd95fcc123fb876a
Paweł Kubrak <peku33@gmail.com>
2015-04-08 21:12:10 +0200

    scons.bat teraz pauzuje tylko gdy wystąpił błąd Skasowałem domyślną ścieżkę do MinGW

--- scons.bat
+++ scons.bat
@@ -1,2 +1,2 @@
-"C:\Python27\Scripts\scons.bat" MinGwPath="C:\Program Files (x86)\mingw-w64\i686-4.9.2-posix-dwarf-rt_v3-rev1\mingw32\bin"
-pause
+"C:\Python27\Scripts\scons.bat"
+if NOT ["%errorlevel%"]==["0"] pause
