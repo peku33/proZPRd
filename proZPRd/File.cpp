bf020c7ddce0b6c07e458b41447be62c13cbc49f
Tomasz Chrzęszczyk <t.chrzeszczyk@gmail.com>
2015-04-10 20:48:58 +0200

    Implementacja metod klasy File

--- proZPRd/File.cpp
+++ proZPRd/File.cpp
@@ -2,11 +2,28 @@
 
 bool proZPRd::File::Exists(const std::string & FileName)
 {
+	struct stat s;
+	if(stat(FileName.c_str(), &s) == 0)
+	{
+		if(s.st_mode & S_IFDIR) return false;
+	}else return false;
 	
+	return true;
 }
 
 std::string proZPRd::File::ToString(const std::string & FileName)
 {
+	std::ifstream t(FileName);
+	std::string str;
 	
+	if(!t.good()) throw Tools::Exception(EXCEPTION_PARAMS, "BLAD: Plik nie istnieje!");
+		
+	t.seekg(0, std::ios::end);   
+	str.reserve(t.tellg());
+	t.seekg(0, std::ios::beg);
+
+	str.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+	
+	return str;
 }
 
